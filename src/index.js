29e73cbe7291bfee35d09f2f35515a7b42248b63
Oracionspace <shipfriend4371@naver.com>
2024-01-10 00:21:28 +0900

    fixed: routing fix

--- src/index.js
+++ src/index.js
@@ -8,14 +8,4 @@ import routes from "./Routes";
 const router = createBrowserRouter(routes);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <RouterProvider router={router}>
-    <Router basename="/primitive">
-      <Routes>
-        {routes.map((router) => (
-          <Route key={router.path} path={router.path} element={router.element} />
-        ))}
-      </Routes>
-    </Router>
-  </RouterProvider>
-);
+root.render(<RouterProvider router={router} />);
