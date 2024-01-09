db557f2e2989af12c3119dcf53f0c70a4b6c45f5
Oracionspace <shipfriend4371@naver.com>
2024-01-10 00:01:46 +0900

    fixed: 멤버 페이지 수정

--- src/Pages/MembersPage.js
+++ src/Pages/MembersPage.js
@@ -11,8 +11,8 @@ const MembersPage = () => {
         <div className="w-screen min-h-screen flex flex-col items-center">
           <h1 className="text-5xl mt-24 mb-10">운영진</h1>
           <div className="flex flex-row gap-4">
-            <MemberCard name={"이재완"} number={19} position={"회장"} description={"오..."} />
-            <MemberCard name={"윤가은"} number={21} position={"부회장"} description={"오....?"} />
+            <MemberCard name={"이 모씨"} number={19} position={"회장"} description={"오..."} />
+            <MemberCard name={"윤 모씨"} number={21} position={"부회장"} description={"오....?"} />
           </div>
         </div>
       </>
