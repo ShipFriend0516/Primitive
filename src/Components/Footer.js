915aca3844bf2853d786523141f61a2cd66e01ff
Oracionspace <shipfriend4371@naver.com>
2024-01-21 02:06:06 +0900

    feat: 푸터 구현 및 링크

---
+++ src/Components/Footer.js
@@ -0,0 +1,72 @@
+// Footer.js
+
+import React from "react";
+import { FaInstagram, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-xl font-bold">Primitive Contact</h2>
+            <p>다른 활동도 확인해보세요!</p>
+          </div>
+          <div className="flex">
+            {/* 소셜 미디어 아이콘 등을 추가하세요 */}
+            <a
+              href="https://www.instagram.com/primitive_knu/"
+              className="text-gray-300 hover:text-white ml-2"
+            >
+              <i className="fab">
+                <FaInstagram style={{ width: 24 + "px", height: 24 + "px" }} />
+              </i>
+            </a>
+            <a
+              href="https://github.com/ShipFriend0516"
+              className="text-gray-300 hover:text-white ml-2"
+            >
+              <i className="fab">
+                <FaGithub style={{ width: 24 + "px", height: 24 + "px" }} />
+              </i>
+            </a>
+            <a
+              href="https://cafe.naver.com/primitive315"
+              className="text-gray-300 hover:text-white ml-2"
+            >
+              <i className="fab">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_403_243)">
+                    <path
+                      d="M18 20H2C0.9 20 0 19.1 0 18V2C0 0.9 0.9 0 2 0H18C19.1 0 20 0.9 20 2V18C20 19.1 19.1 20 18 20Z"
+                      fill="#03C75A"
+                    />
+                    <path
+                      d="M11.35 10.25L8.50002 6.19995H6.15002V13.8H8.65002V9.74995L11.5 13.8H13.85V6.19995H11.35V10.25Z"
+                      fill="white"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_403_243">
+                      <rect width="20" height="20" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </i>
+            </a>
+          </div>
+        </div>
+        <hr className="my-4 border-gray-600" />
+        <p className="text-center">© 2024 Primitive 동아리. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,11 +1,13 @@
 import NavBar from "../Components/NavBar";
 import "../Styles/animations.css";
 import Intro from "../Components/Intro";
+import Footer from "../Components/Footer";
 const MainPage = () => {
   return (
     <>
       <NavBar />
       <Intro />
+      <Footer />
     </>
   );
 };
