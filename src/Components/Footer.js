f2d565898ba5fbce62b506423e9a51f27937923d
Jinseong <hdss8234@gmail.com>
2024-01-29 20:11:31 +0900

    이미지 수정 by BinarySstar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -70,7 +70,6 @@ const Footer = () => {
           </div>
         </div>
         <hr className="my-4 border-gray-600"/>
-        <p className="text-center">충청남도 천안시 서북구 천안대로 1223-24 공주대학교 학생회관 315호</p>
         <p className="text-center">Copyright© 2024 Primitive. All rights reserved.</p>
       </div>
     </footer>
