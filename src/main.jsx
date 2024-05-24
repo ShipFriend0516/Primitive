a374ffd76e14d0996799db7a3a6109dff3992b3c
Oracionspace <shipfriend4371@naver.com>
2024-05-24 10:38:54 +0900

    fix: tailwindcss 적용 오류 해결

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import { RouterProvider, Routes } from "react-router-dom";
 import { BrowserRouter as Router, Route, createBrowserRouter } from "react-router-dom";
 import routes from "./Routes";
 
 const router = createBrowserRouter(routes);
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RouterProvider router={router} />);
+
+ReactDOM.render(<RouterProvider router={router} />, document.getElementById('root'));
