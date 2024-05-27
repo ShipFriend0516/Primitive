f764ea040cd305d8abeb74f456bbdd23249ef03f
Oracionspace <shipfriend4371@naver.com>
2024-05-27 14:50:02 +0900

    fix: 프로젝트 사이 간격 축소

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -22,7 +22,7 @@ const ProjectCard = ({
   };
 
   return (
-    <div className="p-2 w-full h-full">
+    <div className="p-2 w-full h-full mb-5">
       <div
         className={`w-full rounded-lg aspect-video overflow-hidden  ${
           isEmpty ? "bg-emerald-100" : ""
@@ -39,7 +39,7 @@ const ProjectCard = ({
           <div onClick={onClickProject} className="cursor-pointer aspect-video"></div>
         )}
       </div>
-      <div className="w-full aspect-video p-0.5">
+      <div className="w-full h-1/2 p-0.5">
         <p className="text-indigo-400 text-sm">{projectDate}</p>
         <p className=" cursor-pointer text-black text-xl" onClick={onClickProject}>
           {projectName}
