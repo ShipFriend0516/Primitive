95a45be08e898528ff03613c78be33da4b51d561
Oracionspace <shipfriend4371@naver.com>
2024-06-20 14:29:05 +0900

    fix: 로그인 로딩 방식 변경

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -269,7 +269,21 @@ const LoginPage = () => {
             className="authBtn flex justify-center items-center"
           >
             {isLogin ? `${loginLoading ? "" : "로그인"}` : `${loginLoading ? "" : "회원가입"}`}
-            {loginLoading && <div className="loader-small text-transparent">Loading...</div>}
+            {loginLoading && (
+              <div className="inline-flex text-rose-100 opacity-90">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 256 256"
+                  className="h-6 mr-1 animate-spin"
+                >
+                  <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
+                </svg>
+                Loading...
+              </div>
+            )}
           </button>
           <div className="p-1 text-left text-red-500 text-sm">{error}</div>
           <div className="p-1 text-left text-green-500 text-sm">{message}</div>
