a2dd4394567b3845016a6250dcd9782f94c3a390
JeongwooSeo <shipfriend4371@naver.com>
2025-01-30 02:40:59 +0900

    refactor: 안쓰는 import 제거

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
