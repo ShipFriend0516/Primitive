ed9a5aadce2e77d74f487bc35c0b51282d543826
JeongwooSeo <shipfriend4371@naver.com>
2025-01-31 20:36:01 +0900

    refactor: 제작 -> 개발로 텍스트 변경

--- src/Pages/MembersPage/MembersPage.tsx
+++ src/Pages/MembersPage/MembersPage.tsx
@@ -2,6 +2,7 @@ import NavBar from "../../Components/common/NavBar";
 import MemberCard from "../../Components/member/MemberCard";
 import Footer from "../../Components/common/Footer";
 import {data, developers} from "@/src/Pages/MembersPage/data";
+import { FaDev} from "react-icons/fa";
 
 const MembersPage = () => {
 
@@ -25,7 +26,7 @@ const MembersPage = () => {
               description={`안녕하세요 PRIMITIVE 부회장 ${data.vicePresidentData.grade}학번 ${data.vicePresidentData.name}입니다!`}
             />
           </div>
-          <h2 className="text-3xl font-bold my-3  ">제작</h2>
+          <h2 className="text-3xl font-bold my-3 inline-flex items-center gap-1"><FaDev/>개발</h2>
           <div className="flex md:flex-row  flex-col gap-2">
             {developers.map((developer, index) => (
             <MemberCard name={developer.name} number={developer.grade} position={developer.position} description={developer.description} image={developer.image} type={"small"} key={index} />
