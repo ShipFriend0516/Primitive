cf6ce4d91179cd01b49e169e8e6bfc7ebb186444
JeongwooSeo <shipfriend4371@naver.com>
2025-02-06 18:13:14 +0900

    refactor: 개발진 ui 변경

--- src/Pages/MembersPage/MembersPage.tsx
+++ src/Pages/MembersPage/MembersPage.tsx
@@ -2,7 +2,7 @@ import NavBar from '../../Components/common/NavBar';
 import MemberCard from '../../Components/member/MemberCard';
 import Footer from '../../Components/common/Footer';
 import { data, developers } from '@/src/Pages/MembersPage/data';
-import { FaDev } from 'react-icons/fa';
+import { FaChevronDown, FaDev } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import Fireworks from '@/src/Components/common/Fireworks';
 
@@ -43,22 +43,25 @@ const MembersPage = () => {
               handleClick={() => handleClick(setEmojiVice)}
             />
           </div>
-          <h2 className='text-3xl font-bold my-3 inline-flex items-center gap-1'>
-            <FaDev />
-            개발
-          </h2>
-          <div className='flex md:flex-row  flex-col gap-2'>
-            {developers.map((developer, index) => (
-              <MemberCard
-                name={developer.name}
-                number={developer.grade}
-                position={developer.position}
-                description={developer.description}
-                image={developer.image}
-                type={'small'}
-                key={index}
-              />
-            ))}
+          <div className={'max-w-7xl '}>
+            <h2 className=' w-full  border-b text-3xl font-bold my-3 inline-flex items-center gap-1 pb-2'>
+              <FaDev />
+              개발
+            </h2>
+
+            <div className='flex md:flex-row  flex-col gap-2'>
+              {developers.map((developer, index) => (
+                <MemberCard
+                  name={developer.name}
+                  number={developer.grade}
+                  position={developer.position}
+                  description={developer.description}
+                  image={developer.image}
+                  type={'small'}
+                  key={index}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </>
--- src/Pages/MembersPage/data.ts
+++ src/Pages/MembersPage/data.ts
@@ -1,43 +1,43 @@
-import jeongwoo from "../../Images/members/jeongwoo.webp";
-import gaeun from "../../Images/members/gaeun.webp";
-import jinseong from "../../Images/members/jinseong.webp";
+import jeongwoo from '../../Images/members/jeongwoo.webp';
+import gaeun from '../../Images/members/gaeun.webp';
+import jinseong from '../../Images/members/jinseong.webp';
 export const data = {
-    presidentData: {
-        position: '회장',
-        name: '장준우',
-        kakaoID: 'JJW1102',
-        email: 'jwjang1102@naver.com',
-        grade: 21,
-    },
-    vicePresidentData: {
-        position: '부회장',
-        name: '최세진',
-        kakaoID: 'marin6670',
-        email: 'marin6670@gmail.com',
-        grade: 21,
-    }
-}
+  presidentData: {
+    position: '회장',
+    name: '장준우',
+    kakaoID: 'JJW1102',
+    email: 'jwjang1102@naver.com',
+    grade: 21,
+  },
+  vicePresidentData: {
+    position: '부회장',
+    name: '최세진',
+    kakaoID: 'marin6670',
+    email: 'marin6670@gmail.com',
+    grade: 21,
+  },
+};
 
 export const developers = [
-    {
-        name: '서정우',
-        grade: 21,
-        position: '프론트엔드',
-        description: 'React | Firebase',
-        image: jeongwoo
-    },
-    {
-        name: '이진성',
-        grade: 20,
-        position: '백엔드',
-        description: 'React',
-        image: jinseong
-    },
-    {
-        name: '윤가은',
-        grade: 21,
-        position: 'Special Thanks',
-        description: 'Design 훈수',
-        image: gaeun
-    }
-]
+  {
+    name: '서정우',
+    grade: 21,
+    position: '프론트엔드',
+    description: 'React | Firebase',
+    image: jeongwoo,
+  },
+  {
+    name: '이진성',
+    grade: 20,
+    position: '백엔드',
+    description: 'React',
+    image: jinseong,
+  },
+  {
+    name: '윤가은',
+    grade: 21,
+    position: '프론트엔드',
+    description: 'Design 훈수',
+    image: gaeun,
+  },
+];
