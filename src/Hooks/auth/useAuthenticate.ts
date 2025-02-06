7703e6b17a8ee4ef2797a58e2965b0822849e985
JeongwooSeo <shipfriend4371@naver.com>
2025-02-06 17:20:29 +0900

    feat: login시 userId도 저장하도록 수정

--- src/Hooks/auth/useAuthenticate.ts
+++ src/Hooks/auth/useAuthenticate.ts
@@ -1,34 +1,34 @@
-import { useState } from "react";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import app, { db } from "@/src/firebase";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import useStore from "@/src/store";
+import { useState } from 'react';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import app, { db } from '@/src/firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import useStore from '@/src/store';
 
 const useAuthenticate = () => {
   const { isLoggedIn, login } = useStore();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [checkPassword, setCheckPassword] = useState("");
-  const [studentYear, setStudentYear] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+  const [studentYear, setStudentYear] = useState('');
   const [isLogin, setIsLogin] = useState(true);
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
   const [loginLoading, setLoginLoading] = useState(false);
   const navigate = useNavigate();
 
   // 유효성 검사
   const validateLogin = () => {
     let isValid = true;
-    setError("");
+    setError('');
 
-    if (!email.includes("@")) {
-      setError("유효한 이메일 주소를 입력해주세요.");
+    if (!email.includes('@')) {
+      setError('유효한 이메일 주소를 입력해주세요.');
       isValid = false;
     }
     if (password.length < 8) {
-      setError("비밀번호는 8자리 이상이어야 합니다.");
+      setError('비밀번호는 8자리 이상이어야 합니다.');
       isValid = false;
     }
     return isValid;
@@ -36,31 +36,31 @@ const useAuthenticate = () => {
 
   const validateSignup = () => {
     let isValid = true;
-    setError("");
+    setError('');
 
     if (username.length < 2) {
-      setError("유저네임은 2자리 이상이어야 합니다.");
+      setError('유저네임은 2자리 이상이어야 합니다.');
       isValid = false;
     }
 
     if (!/^\d{2}$/.test(studentYear)) {
-      setError("올바르지 않은 학번 형식입니다. (2자리 숫자)");
+      setError('올바르지 않은 학번 형식입니다. (2자리 숫자)');
       isValid = false;
     }
 
-    if (!email.includes("@")) {
-      setError("유효한 이메일 주소를 입력해주세요.");
+    if (!email.includes('@')) {
+      setError('유효한 이메일 주소를 입력해주세요.');
       isValid = false;
     }
 
     // const pwRegex = /[!@#$%^&*?]/;
     if (password.length < 8) {
-      setError("비밀번호는 8자리 이상이어야합니다.");
+      setError('비밀번호는 8자리 이상이어야합니다.');
       isValid = false;
     }
 
     if (password !== checkPassword) {
-      setError("비밀번호가 일치하지 않습니다.");
+      setError('비밀번호가 일치하지 않습니다.');
       isValid = false;
     }
 
@@ -74,47 +74,47 @@ const useAuthenticate = () => {
       if (validateSignup()) {
         setLoginLoading(true);
         const requestQuery = query(
-          collection(db, "signupRequests"),
-          where("email", "==", email),
+          collection(db, 'signupRequests'),
+          where('email', '==', email),
         );
         const registeredQuery = query(
-          collection(db, "users"),
-          where("email", "==", email),
+          collection(db, 'users'),
+          where('email', '==', email),
         );
 
         const registeredSnapshot = await getDocs(registeredQuery);
         if (!registeredSnapshot.empty) {
           // 이미 유저있을때
-          setError("이미 등록된 이메일입니다.");
+          setError('이미 등록된 이메일입니다.');
           return;
         }
         const querySnapshot = await getDocs(requestQuery);
         if (querySnapshot.empty) {
-          await addDoc(collection(db, "signupRequests"), {
+          await addDoc(collection(db, 'signupRequests'), {
             email: email,
             password: password,
             username: username,
             studentYear: studentYear,
-            status: "pending",
+            status: 'pending',
           });
           setMessage(
-            "가입 요청을 성공적으로 기록했습니다. 요청 확인까지 수 일이 소요될 수 있습니다.",
+            '가입 요청을 성공적으로 기록했습니다. 요청 확인까지 수 일이 소요될 수 있습니다.',
           );
           setIsLogin(!isLogin);
-          setEmail("");
-          setPassword("");
-          setCheckPassword("");
-          setStudentYear("");
-          setUsername("");
+          setEmail('');
+          setPassword('');
+          setCheckPassword('');
+          setStudentYear('');
+          setUsername('');
         } else {
-          setMessage("이미 신청된 요청입니다.");
+          setMessage('이미 신청된 요청입니다.');
         }
       } else {
-        console.log("유효성 검사 실패");
+        console.log('유효성 검사 실패');
       }
     } catch (error) {
       console.error(error);
-      setError("회원가입 과정 중 오류가 발생했습니다.");
+      setError('회원가입 과정 중 오류가 발생했습니다.');
     } finally {
       setLoginLoading(false);
     }
@@ -128,14 +128,14 @@ const useAuthenticate = () => {
         const result = await signInWithEmailAndPassword(auth, email, password);
 
         if (result) {
-          login();
-          navigate("/");
+          login(result.user.uid);
+          navigate('/');
         } else {
-          setError("로그인에 실패했습니다. 다시 시도해주세요.");
+          setError('로그인에 실패했습니다. 다시 시도해주세요.');
         }
       }
     } catch (error) {
-      setError("로그인에 실패했습니다. 다시 시도해주세요.");
+      setError('로그인에 실패했습니다. 다시 시도해주세요.');
       console.error(error);
     } finally {
       setLoginLoading(false);
--- src/store.tsx
+++ src/store.tsx
@@ -1,15 +1,27 @@
-import { UseBoundStore, create } from "zustand";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface LoginStatus {
   isLoggedIn: undefined | boolean;
+  userId: undefined | string;
   logout: () => void;
-  login: () => void;
+  login: (userId: string) => void;
 }
 
-const useStore = create<LoginStatus>((set) => ({
-  isLoggedIn: undefined,
-  login: () => set({ isLoggedIn: true }),
-  logout: () => set({ isLoggedIn: false }),
-}));
+const useStore = create(
+  persist<LoginStatus>(
+    (set) => ({
+      isLoggedIn: undefined,
+      userId: undefined,
+      login: (userId: string) => {
+        set({ isLoggedIn: true, userId: userId });
+      },
+      logout: () => set({ isLoggedIn: false, userId: undefined }),
+    }),
+    {
+      name: 'login-status',
+    },
+  ),
+);
 
 export default useStore;
