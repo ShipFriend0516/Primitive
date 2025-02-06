b061dfda26654e6b3280e4db32c1e6aede8e4b7a
JeongwooSeo <shipfriend4371@naver.com>
2025-02-06 19:05:26 +0900

    feat: 댓글 삭제할 때 모달 창 사용하도록 구현

--- src/Components/project/Comment.tsx
+++ src/Components/project/Comment.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import CommentType from "../../Types/CommentType";
-import LoadingCircle from "../common/LoadingCircle";
-import Reply from "./Reply";
-import { getAuth } from "firebase/auth";
+import { useEffect, useState } from 'react';
+import CommentType from '../../Types/CommentType';
+import LoadingCircle from '../common/LoadingCircle';
+import Reply from './Reply';
+import { getAuth } from 'firebase/auth';
 import {
   addDoc,
   collection,
@@ -14,9 +14,12 @@ import {
   orderBy,
   deleteDoc,
   updateDoc,
-} from "firebase/firestore";
-import { db } from "../../firebase";
-import { useNavigate } from "react-router-dom";
+} from 'firebase/firestore';
+import { db } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import useModal from '@/src/Hooks/common/useModal';
+import ModalLayout from '@/src/Components/common/modal/ModalLayout';
+import DefaultModal from '@/src/Components/common/modal/DefaultModal';
 
 interface Params {
   id: string;
@@ -40,13 +43,13 @@ const Comment = ({
   deleteComment,
 }: Params) => {
   const [isReplyShow, setIsReplyShow] = useState(false);
-  const [reply, setReply] = useState("");
+  const [reply, setReply] = useState('');
   const [replies, setReplies] = useState<CommentType[]>();
   const [replyLoading, setReplyLoading] = useState(true);
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState('');
   const [isEditing, setIsEditing] = useState(false);
-  const [commentInput, setCommentInput] = useState("");
-
+  const [commentInput, setCommentInput] = useState('');
+  const commentDeleteModalInstance = useModal();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -70,7 +73,7 @@ const Comment = ({
         return false;
       }
     } catch (err) {
-      console.error("유저검증실패", err);
+      console.error('유저검증실패', err);
     }
   };
 
@@ -84,16 +87,16 @@ const Comment = ({
           comment: reply,
           createdAt: new Date().getTime(),
         };
-        await addDoc(collection(db, "comments"), commentData);
-        setReply("");
+        await addDoc(collection(db, 'comments'), commentData);
+        setReply('');
         getReply(comment_id);
       } else {
         // 유저가 아니라면
-        alert("로그인이 필요한 서비스입니다.");
-        navigate("/login");
+        alert('로그인이 필요한 서비스입니다.');
+        navigate('/login');
       }
     } catch (e) {
-      console.error("답글 작성 중 오류", e);
+      console.error('답글 작성 중 오류', e);
     }
   };
 
@@ -101,35 +104,33 @@ const Comment = ({
     try {
       const response = await getDocs(
         query(
-          collection(db, "comments"),
-          where("projectId", "==", comment_id),
-          orderBy("createdAt")
-        )
+          collection(db, 'comments'),
+          where('projectId', '==', comment_id),
+          orderBy('createdAt'),
+        ),
       );
 
       const userdatas = await Promise.all(
         response.docs.map(async (document) => {
           const userId = document.data().authorId;
-          const userRef = doc(db, "users", userId);
+          const userRef = doc(db, 'users', userId);
           const userDoc = await getDoc(userRef);
-          return (
-            {
-              username: userDoc.data()?.username,
-              profileThumbnail: userDoc.data()?.profileThumbnail,
-            } || "Unknown User"
-          );
-        })
+          return {
+            username: userDoc.data()?.username,
+            profileThumbnail: userDoc.data()?.profileThumbnail,
+          };
+        }),
       );
 
       const data = await Promise.all(
         response.docs.map(async (doc, index) => {
           return {
             id: doc.id,
-            ...(doc.data() as Omit<CommentType, "id">),
+            ...(doc.data() as Omit<CommentType, 'id'>),
             username: userdatas[index].username as string,
             profileThumbnail: userdatas[index].profileThumbnail as string,
           };
-        })
+        }),
       );
 
       setReplies(data);
@@ -141,7 +142,7 @@ const Comment = ({
 
   const deleteReply = async (reply_id: string) => {
     try {
-      const commentRef = doc(db, "comments", reply_id);
+      const commentRef = doc(db, 'comments', reply_id);
       await deleteDoc(commentRef);
       setReplies((prev) => prev?.filter((reply) => reply.id !== reply_id));
     } catch (err) {
@@ -152,7 +153,7 @@ const Comment = ({
   const replyRender = () => {
     return (
       isReplyShow && (
-        <div className="bg-gray-100 p-3">
+        <div className='bg-gray-100 p-3'>
           {replies!.map((reply) => (
             <Reply
               id={reply.id}
@@ -168,12 +169,12 @@ const Comment = ({
           <textarea
             value={reply}
             onChange={(e) => setReply(e.target.value)}
-            className="border p-4 w-full"
-            placeholder="답글을 남겨보세요"
+            className='border p-4 w-full'
+            placeholder='답글을 남겨보세요'
           />
-          <div className="flex justify-end">
+          <div className='flex justify-end'>
             <button
-              className="px-3 py-1  bg-black text-white rounded-md"
+              className='px-3 py-1  bg-black text-white rounded-md'
               onClick={() => {
                 postReply(id);
               }}
@@ -189,31 +190,39 @@ const Comment = ({
   return replyLoading ? (
     <LoadingCircle />
   ) : (
-    <div className="w-full flex flex-col justify-center gap-3 border-b p-3">
-      <div className="flex flex-row items-center gap-3">
+    <div className='w-full flex flex-col justify-center gap-3 border-b p-3'>
+      <div className='flex flex-row items-center gap-3'>
         <div>
-          <div className="rounded-full w-12 h-12 bg-gray-300 overflow-hidden">
+          <div className='rounded-full w-12 h-12 bg-gray-300 overflow-hidden'>
             {thumbnailUrl && <img src={thumbnailUrl} alt={username} />}
           </div>
         </div>
-        <div className="flex flex-col flex-grow">
-          <div className="font-bold">{username}</div>
-          <div className="font-light text-sm">{new Date(createdAt).toLocaleDateString()}</div>
+        <div className='flex flex-col flex-grow'>
+          <div className='font-bold'>{username}</div>
+          <div className='font-light text-sm'>
+            {new Date(createdAt).toLocaleDateString()}
+          </div>
         </div>
         {isOwner && (
-          <div className="text-sm text-gray-500">
+          <div className='text-sm text-gray-500'>
             {!isEditing && (
               <button
                 onClick={() => {
                   setIsEditing(true);
                   setCommentInput(comment);
                 }}
-                className="mr-1"
+                className='mr-1'
               >
                 수정
               </button>
             )}
-            <button onClick={() => deleteComment(id)}>삭제</button>
+            <button
+              onClick={() => {
+                commentDeleteModalInstance.toggleModal();
+              }}
+            >
+              삭제
+            </button>
           </div>
         )}
       </div>
@@ -222,18 +231,18 @@ const Comment = ({
           <textarea
             value={commentInput}
             onChange={(e) => setCommentInput(e.target.value)}
-            className="border p-4 w-full"
-            placeholder="댓글 수정 중..."
+            className='border p-4 w-full'
+            placeholder='댓글 수정 중...'
           />
-          <div className="w-full inline-flex justify-end">
+          <div className='w-full inline-flex justify-end'>
             <button
-              className="px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded-md mr-2"
+              className='px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded-md mr-2'
               onClick={() => setIsEditing(false)}
             >
               취소
             </button>
             <button
-              className="px-2 py-1 bg-indigo-600 hover:bg-indigo-700 text-white rounded-md"
+              className='px-2 py-1 bg-indigo-600 hover:bg-indigo-700 text-white rounded-md'
               onClick={() => {
                 updateComment(id, commentInput);
                 setIsEditing(false);
@@ -244,10 +253,10 @@ const Comment = ({
           </div>
         </div>
       ) : (
-        <div className="content">{comment}</div>
+        <div className='content'>{comment}</div>
       )}
       <div
-        className="text-emerald-500 cursor-pointer"
+        className='text-emerald-500 cursor-pointer'
         onClick={() => {
           setIsReplyShow(!isReplyShow);
         }}
@@ -255,6 +264,19 @@ const Comment = ({
         {replies!.length || 0}개의 답글
       </div>
       {replyRender()}
+      <ModalLayout
+        ref={commentDeleteModalInstance.modalRef}
+        isOpen={commentDeleteModalInstance.isOpen}
+        setIsOpen={commentDeleteModalInstance.setIsOpen}
+      >
+        <DefaultModal
+          title={'댓글 삭제'}
+          subTitle={'댓글을 삭제할까요?'}
+          toggleModal={commentDeleteModalInstance.toggleModal}
+          confirm={() => deleteComment(id)}
+          cancel={commentDeleteModalInstance.toggleModal}
+        />
+      </ModalLayout>
     </div>
   );
 };
--- src/Components/project/Reply.tsx
+++ src/Components/project/Reply.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import CommentType from "../../Types/CommentType";
-import LoadingCircle from "../common/LoadingCircle";
+import DefaultModal from '@/src/Components/common/modal/DefaultModal';
+import ModalLayout from '@/src/Components/common/modal/ModalLayout';
+import useModal from '@/src/Hooks/common/useModal';
 
 interface Params {
   id: string;
@@ -12,29 +12,56 @@ interface Params {
   deleteReply: (id: string) => Promise<void>;
 }
 
-const Reply = ({ id, username, reply, thumbnailUrl, isOwner, createdAt, deleteReply }: Params) => {
+const Reply = ({
+  id,
+  username,
+  reply,
+  thumbnailUrl,
+  isOwner,
+  createdAt,
+  deleteReply,
+}: Params) => {
+  const replyDeleteModalInstance = useModal();
   return (
-    <div className="w-full flex flex-col justify-center gap-3 border-b p-3">
-      <div className="flex flex-row items-center gap-3">
+    <div className='w-full flex flex-col justify-center gap-3 border-b p-3'>
+      <div className='flex flex-row items-center gap-3'>
         <div>
-          <div className="rounded-full w-12 h-12 bg-gray-300 overflow-hidden">
+          <div className='rounded-full w-12 h-12 bg-gray-300 overflow-hidden'>
             {thumbnailUrl && <img src={thumbnailUrl} alt={username} />}
           </div>
         </div>
-        <div className="flex flex-col flex-grow">
-          <div className="font-bold">{username}</div>
-          <div className="font-light text-sm">{new Date(createdAt).toLocaleDateString()}</div>
+        <div className='flex flex-col flex-grow'>
+          <div className='font-bold'>{username}</div>
+          <div className='font-light text-sm'>
+            {new Date(createdAt).toLocaleDateString()}
+          </div>
         </div>
         {isOwner && (
-          <div className="text-sm text-gray-500">
-            <button className="mr-1">수정</button>
-            <button className="" onClick={() => deleteReply(id)}>
+          <div className='text-sm text-gray-500'>
+            <button className='mr-1'>수정</button>
+            <button
+              className=''
+              onClick={() => replyDeleteModalInstance.toggleModal()}
+            >
               삭제
             </button>
           </div>
         )}
       </div>
-      <div className="content">{reply}</div>
+      <div className='content'>{reply}</div>
+      <ModalLayout
+        ref={replyDeleteModalInstance.modalRef}
+        isOpen={replyDeleteModalInstance.isOpen}
+        setIsOpen={replyDeleteModalInstance.setIsOpen}
+      >
+        <DefaultModal
+          title={'답글 삭제'}
+          subTitle={'답글을 삭제할까요?'}
+          toggleModal={replyDeleteModalInstance.toggleModal}
+          confirm={() => deleteReply(id)}
+          cancel={replyDeleteModalInstance.toggleModal}
+        />
+      </ModalLayout>
     </div>
   );
 };
