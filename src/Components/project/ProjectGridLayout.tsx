371e50ddb1bdb7c4e4e606d09e0743c45cab6124
JeongwooSeo <shipfriend4371@naver.com>
2025-02-07 16:53:01 +0900

    feat: project page 리뉴얼된 UI 모바일 반응성 향상

--- src/Components/project/ProjectGridLayout.tsx
+++ src/Components/project/ProjectGridLayout.tsx
@@ -6,7 +6,7 @@ const ProjectGridLayout = ({ children }: ProjectGridLayoutProps) => {
   return (
     <div
       id='projectGrid'
-      className='w-4/5 mx-20 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3'
+      className='w-full sm:w-4/5 px-4 sm:px-0 mx-10 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3'
     >
       {children}
     </div>
