8ded4fb10174cbc3b2ff77958adc8bc1d165459f
JeongwooSeo <shipfriend4371@naver.com>
2025-03-11 01:27:58 +0900

    fix: 마감일을 포함하도록 수정

--- src/Pages/RecruitPage/RecruitPage.tsx
+++ src/Pages/RecruitPage/RecruitPage.tsx
@@ -14,7 +14,7 @@ const RecruitPage = () => {
   useEffect(() => {
     const now = new Date();
     const start = new Date(2025, 1, 10);
-    const end = new Date(2025, 2, 14);
+    const end = new Date(2025, 2, 11, 23, 59, 59);
     const weeks = ['일', '월', '화', '수', '목', '금', '토'];
 
     setTimeString(
